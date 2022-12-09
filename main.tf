87abffd6a2a805170c9c9376184576f5b7b0cb5d
Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>
2022-12-09 12:08:22 +0100

    IaCBot fix
    Signed-off-by: Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>

--- main.tf
+++ main.tf
@@ -50,11 +50,12 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "virtual_machine" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.resource_group[0].name
-  location            = azurerm_resource_group.resource_group[0].location
-  size                = var.vm_size
-  admin_username      = local._admin_name
+  name                       = var.vm_name
+  resource_group_name        = azurerm_resource_group.resource_group[0].name
+  location                   = azurerm_resource_group.resource_group[0].location
+  size                       = var.vm_size
+  admin_username             = local._admin_name
+  allow_extension_operations = false
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
