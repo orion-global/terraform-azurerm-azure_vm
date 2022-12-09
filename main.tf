8b57fdc690e017bb810fe5f3d1c93f4803650d95
Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>
2022-12-09 11:30:31 +0100

    Update nic reference
    Signed-off-by: Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = data.azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
