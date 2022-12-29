23b9a09281ee2deb4775ad62cb28e17179d7e069
Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>
2022-12-29 15:03:04 -0500

    feat: Added VM output id
    Signed-off-by: Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,7 @@ output "private_ssh_key" {
   value     = var.vm_type == "Linux" && var.create_linux_key ? tls_private_key.virtual_machine_ssh_key[0].private_key_openssh : null
   sensitive = true
 }
+
+output "virtual_machine_id" {
+  value = var.vm_type == "Linux" ? azurerm_linux_virtual_machine.virtual_machine[0].id : azurerm_windows_virtual_machine.virtual_machine[0].id
+}
